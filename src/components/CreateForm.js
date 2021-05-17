80bd7810a5ae4c5bf5e0aa384db02b096f9d8b03
tsamuel1289 <52178585+tsamuel1289@users.noreply.github.com>
2021-05-17 15:03:34 -0400

    Modificado en sandbox

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const CreateForm = () => {
-    return (
-        <>
-            <h3>Formulario de Canciones</h3>
-            <form>
-                <input 
-                    className='inputsCreate'
-                    type="text"
-                    name="name-song"
-                    placeholder='Name of song'
-                     />
+  return (
+    <>
+      <h3>Formulario de Canciones</h3>
+      <form>
+        <input
+          className="inputsCreate"
+          type="text"
+          name="name-song"
+          placeholder="Name of song"
+        />
 
-                <input 
-                    className='inputsCreate'
-                    type="text"
-                    name='artist-song'
-                    placeholder='Artist'
-                 />
-                 <textarea 
-                    className='inputsCreate'
-                    name="lyric-song"
-                    placeholder='Artist'
-                    rows="5"></textarea>
-
-            </form>
-        </>
-    )
-}
-export default CreateForm
+        <input
+          className="inputsCreate"
+          type="text"
+          name="artist-song"
+          placeholder="Artist"
+        />
+        <textarea
+          className="inputsCreate"
+          name="lyric-song"
+          placeholder="Write the song letter"
+          rows="5"
+        ></textarea>
+      </form>
+    </>
+  );
+};
+export default CreateForm;
