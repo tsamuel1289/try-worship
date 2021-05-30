c4191b204d92f376a7db39671f3ddb0d37458a45
samuel <romerotitosamuel@gmail.com>
2021-05-30 17:00:07 -0400

    up

--- src/components/ShowSongs.js
+++ src/components/ShowSongs.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {db} from '../firebase'
 
 const ShowSongs = () => {
+
+    const [songs, setSongs] = useState([])
+
+    const getSongs = () => { //50:07 get songs from firebase
+        db.collection('songs').onSnapshot(
+            (querySnapshot) =>  querySnapshot.forEach( 
+                docs=[];
+                (doc)=> {
+                    docs=[];
+                    docs
+                    console.log(doc.data())
+                }   
+
+                
+            )
+        )
+        
+
+    }
+    
+
+
+    useEffect( ()=>{ getSongs() }  ,[])
+
     return <div>
         <h1>muestra canciones</h1>
+       
+        })}
     </div>
 }
 export default ShowSongs
