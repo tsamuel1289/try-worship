0a4b6548e9f4c788297624088ab6bd145df9428f
samuel <romerotitosamuel@gmail.com>
2021-08-11 22:25:46 -0400

    update

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {db} from '../firebase'
 
 const Add = () =>{
     
+    const addsong = () => {
+        
+    }
 
     return(
-        <h2>Esto es Add</h2>
-
+        <div>
+            <h2>Esto es Add</h2>
+            <form onSubmit={addsong}>
+                <input type="text" />
+                <button>add</button>
+            </form>
+        </div>
     )
 }
 export default Add
